b41f099f3fc10d4a43736d32c22602891a220189
3e13d2bb-c2a8-43bc-9e2e-6e2a83ad8028 <n.urazov@belfood.by>
2024-10-21 14:12:55 +0300

    Добавлено поле updated_at и unique_key

--- models/facts/fact_Продажи.sql
+++ models/facts/fact_Продажи.sql
@@ -2,4 +2,6 @@
     materialized='table'
 ) }}
 
-SELECT * FROM {{ ref('stg_РН_Продажи') }}
+SELECT *,
+concat(toString(`НомерСтроки`),toString(`РегистраторГуид`)) as unique_key, 
+now() as updated_at FROM {{ ref('stg_РН_Продажи') }}
