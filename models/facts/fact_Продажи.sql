690e07bcb5a713b73897e60fcb2b935081171626
3e13d2bb-c2a8-43bc-9e2e-6e2a83ad8028 <n.urazov@belfood.by>
2024-10-22 09:23:59 +0300

    Факт продаж материализован инкрементально

--- models/facts/fact_Продажи.sql
+++ models/facts/fact_Продажи.sql
@@ -1,7 +1,14 @@
-{{ config(
-    materialized='table'
-) }}
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
 
 SELECT *,
 concat(toString(`НомерСтроки`),toString(`РегистраторГуид`)) as unique_key, 
 now() as updated_at FROM {{ ref('stg_РН_Продажи') }}
+
+{% if is_incremental() %}
+  where "Период" >= dateAdd(month, -3, today())
+{% endif %}
