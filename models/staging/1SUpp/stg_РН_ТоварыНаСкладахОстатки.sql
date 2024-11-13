3201ede67bc63e855da1f7cbc67899ea0f9db8e0
3e13d2bb-c2a8-43bc-9e2e-6e2a83ad8028 <n.urazov@belfood.by>
2024-11-14 00:54:53 +0300

    Оптимизирован stg_РН_ТоварыНаСкладахОстатки

--- models/staging/1SUpp/stg_РН_ТоварыНаСкладахОстатки.sql
+++ models/staging/1SUpp/stg_РН_ТоварыНаСкладахОстатки.sql
@@ -17,12 +17,30 @@ signed as
     ,concat(to_char("ДатаОстатка", 'DD-MM-YYYY HH:MI:SS'), "СерияНоменклатурыГуид","ХарактеристикаНоменклатуры", "КачествоГуид", "НоменклатураГуид", "СкладГуид") as key_record
     ,now() as updated_at 
     FROM source
-) 
+),
 
-select * from signed
+columns_order_data_types as
+(
+    select 
+    CAST("Склад" AS VARCHAR(80)) as "Склад", 
+    CAST("Номенклатура" AS VARCHAR(150)) as "Номенклатура", 
+    CAST("Качество" AS VARCHAR(50)) as "Качество", 
+    CAST("ХарактеристикаНоменклатуры" AS VARCHAR(50)) as "ХарактеристикаНоменклатуры", 
+    CAST("СерияНоменклатуры" AS VARCHAR(70)) as "СерияНоменклатуры",
+    CAST(key_record AS VARCHAR(180)) as key_record,
+    "СкладГуид", 
+    "НоменклатураГуид", 
+    "КачествоГуид", 
+    "СерияНоменклатурыГуид",
+    "ДатаОстатка",
+    updated_at
+    "КоличествоОстаток"
+    from signed
+)
+
+select * from columns_order_data_types
 
 
 {% if is_incremental() %}
   where "ДатаОстатка"::date >= CURRENT_DATE - interval '3 days'
 {% endif %}
-
