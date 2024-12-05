3a6309632d867b6d0479daf73f721c3b205db1f0
3e13d2bb-c2a8-43bc-9e2e-6e2a83ad8028 <n.urazov@belfood.by>
2024-12-05 10:23:32 +0300

    Продажи новинок по беларуси

--- models/consolidation/reports/rpt_sales_new_prods.sql
+++ models/consolidation/reports/rpt_sales_new_prods.sql
@@ -10,7 +10,18 @@ filtred as
     WHERE
     "Дата первой продажи по артикулу" ::date >= CURRENT_DATE - interval '6 months'
     or "Статус номенклат." ='Новинка'
+),
+
+filtred2 as
+(
+    SELECT * 
+    FROM filtred
+    WHERE
+        COALESCE("Категория направления", '') IN ('Беларусь', 'e-commerce РБ')
+        AND COALESCE("Категория направления", '') NOT LIKE '%pack%'
+        AND COALESCE("Регион торгового объекта", '') NOT IN ('Монголия')
+        AND COALESCE("Тип канала контрагента", '') != 'Экспорт'
 )
 
 SELECT * 
-FROM filtred
+FROM filtred2
