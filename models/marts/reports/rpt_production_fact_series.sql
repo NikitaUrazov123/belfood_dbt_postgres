7aa061c97fc4a5fe04b90e8e2a933f8e8f644ed6
3e13d2bb-c2a8-43bc-9e2e-6e2a83ad8028 <n.urazov@belfood.by>
2024-12-11 12:10:14 +0300

    Отфилтиррованы материалы в производстве

--- models/marts/reports/rpt_production_fact_series.sql
+++ models/marts/reports/rpt_production_fact_series.sql
@@ -4,5 +4,6 @@ base as
     select
     {{star_exclude_guid(ref('mrt_production_fact_series'), additional_excludes=["record_id"])}}
     from {{ ref("mrt_production_fact_series") }}
+    where "Вид номенклат." <> 'Материалы'
 )
 select * from base
